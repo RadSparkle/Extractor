3a450d7c8980b6f660a433c2296b2493f8f49444
RadSparkle <wwwartg1@naver.com>
2024-03-06 21:56:07 +0900

    Initial commit

---
+++ src/main/java/com/investor/extractor/InvestorHubExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.investor.extractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestorHubExtractorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestorHubExtractorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/investor/extractor/InvestorHubExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investor.extractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestorHubExtractorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
